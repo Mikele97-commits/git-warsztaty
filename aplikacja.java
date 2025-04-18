6711cf43fd232c79abc979066f622de760dfd8c7
Mikele97-commits <michal.minich@o2.pl>
2025-04-18 23:08:05 +0200

    Utworzenie klasy Write

--- aplikacja.java
+++ aplikacja.java
@@ -1,5 +1,13 @@
+import java.util.Scanner; 
+
 class Main {
     public static void main(String[] args) {
+        Write.basicWrite();
+    }
+}
+
+class Write {
+    public static void basicWrite(){
         System.out.println("Hello World!");
     }
 }
