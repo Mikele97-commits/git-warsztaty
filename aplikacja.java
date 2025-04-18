fc968f01c56163985e068d895f2e701ffb398967
Mikele97-commits <michal.minich@o2.pl>
2025-04-18 23:20:54 +0200

    Utworzenie klasy Choice pozwalającej w przyszlości wprowadzić input wybierający wersję wypisania

--- aplikacja.java
+++ aplikacja.java
@@ -1,10 +1,19 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Write.basicWrite();
     }
 }
+
+class Choice {
+        public static int input(){
+            Scanner scan=new Scanner(System.in);
+            System.out.println("Daj input");
+            int input=scan.nextInt();
+            return input;
+        }   
+}
+
 
 class Write {
     public static void basicWrite(){
