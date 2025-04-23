eb9bd8daedeba81bcc16c81c05d6974f479f72de
Marcella Ciempa <marcella.ciempa@gmail.com>
2025-04-23 11:51:02 +0200

    Dodanie metody inverse

--- aplikacja.java
+++ aplikacja.java
@@ -8,22 +8,30 @@ class Main {
 }
 
 class Choice {
-        public static int input(){
-            Scanner scan=new Scanner(System.in);
-            System.out.println("Daj input");
-            int input=scan.nextInt();
-            return input;
-        }   
+    public static int input(){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Daj input");
+        int input=scan.nextInt();
+        return input;
+    }
 }
 
 class Write {
     public static void options(int input){
         switch(input){
             case 1: Write.standard();
+            break;
+            case 2: Write.inverse();
+            break;
         }
     }
-    
+
     public static void standard(){
         System.out.println("Hello World!");
+    }
+
+
+    public static void inverse() {
+        System.out.println("!dlroW olleH");
     }
 }
